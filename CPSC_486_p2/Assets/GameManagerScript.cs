288d761836e6cffd39ce6115b6fd9d96be837035
DJDomingo <djdomingo71@gmail.com>
2018-12-01 22:20:35 -0800

    Upload of Files
    Unity Project upload which consists of a basic scene containing a ball and box.  GameManager script emulates a slower gravity causing the objects to fall. Need to implement rotation and collision from scratch.

---
+++ CPSC_486_p2/Assets/GameManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public float gravity = 0.12f; // m/s^2
+	public GameObject ball;
+	public GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ball.transform.Translate( Vector2.down*gravity*Time.deltaTime, Space.World);
+		box.transform.Translate( Vector2.down*gravity*Time.deltaTime, Space.World);
+	}
+
+
+
+}
---
+++ README.txt
@@ -0,0 +1,23 @@
+Program Name: BlockFall Project#1
+By:     Daniel Domingo (djdomingo@csu.fullertin.edu), 
+	Jordan Harvey (jpharvey@csu.fullerton.edu), 
+	Patrick Myers (pmyers@csu.fullerton.edu),
+	Kim-Lan Hoang (khoangl@csu.fullerton.edu)
+	 
+Team Name: ZZZ
+Class #: 486
+
+Files:  - 
+
+Intro: 
+
+External Req: 
+
+Build: Used Visual Studio and coded with C++.  
+Usage:  Currently project has:
+	- 
+	Still needs:
+	- 
+
+Extra Features: none
+Bugs: 
