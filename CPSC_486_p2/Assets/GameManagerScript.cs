76f227fa261301a7bb096e5f463202d3cd6a02da
DJDomingo <djdomingo71@gmail.com>
2018-12-05 19:32:30 -0800

    Ball and Box collision
    Currently both objects now move and can collide and bounce off one another.  Does NOT use rotation atm and corners can be funky.

--- CPSC_486_p2/Assets/GameManagerScript.cs
+++ CPSC_486_p2/Assets/GameManagerScript.cs
@@ -14,10 +14,12 @@ public class GameManagerScript : MonoBehaviour {
 	public GameObject box;
 
 	private Vector2 ballDirection;
+	private Vector2 boxDirection;
 
 	// Use this for initialization
 	void Start () {
-		ballDirection = new Vector2 (0, -1); // start by moving object straight down
+		ballDirection = new Vector2 (2, -1); // start by moving object straight down
+		boxDirection = new Vector2 (-2, -1); // start by moving object straight down
 		radius = ball.GetComponent<SphereCollider>().radius;
 		screenHeight = 2f * cam.orthographicSize;
 		screenWidth = screenHeight * cam.aspect;
@@ -27,9 +29,9 @@ public class GameManagerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		ball.transform.Translate( ballDirection*gravity*Time.deltaTime, Space.World);
-		//box.transform.Translate( Vector2.down*gravity*Time.deltaTime, Space.World);
+		box.transform.Translate( boxDirection*gravity*Time.deltaTime, Space.World);
 
-		//Movement for when ball hits edge of Screen
+		//Movement for when ball hits edges of Screen
 		if (ball.transform.position.y - radius < -screenHeight/2 || ball.transform.position.y + radius > screenHeight/2) {
 			ballDirection = new Vector2 (ballDirection.x, -ballDirection.y);
 		}
@@ -37,14 +39,44 @@ public class GameManagerScript : MonoBehaviour {
 			ballDirection = new Vector2 (-ballDirection.x, ballDirection.y);
 		}
 
-		//Movement for ball hitting other objects
-		if (box.transform.position.x < ball.transform.position.x + radius &&
-			box.transform.position.x + 3 > ball.transform.position.x &&
-			box.transform.position.y < ball.transform.position.y + radius &&
-			box.transform.position.y + 0.5 > ball.transform.position.y) {
-
-			Debug.Log ("Collision!");
+		//Movement for when box hits edges of Screen
+		if (box.transform.position.y - box.transform.localScale.y/2 < -screenHeight/2 || box.transform.position.y + box.transform.localScale.y/2 > screenHeight/2) {
+			boxDirection = new Vector2 (boxDirection.x, -boxDirection.y);
 		}
+		if (box.transform.position.x - box.transform.localScale.x/2 < -screenWidth/2 || box.transform.position.x + box.transform.localScale.x/2 > screenWidth/2) {
+			boxDirection = new Vector2 (-boxDirection.x, boxDirection.y);
+		}
+			
+		//Ball is colliding with box (CURRENTLY MESSES UP ON CORNERS)
+		if (ball.transform.position.x + radius > box.transform.position.x - box.transform.localScale.x/2 &&
+			ball.transform.position.x - radius < box.transform.position.x + box.transform.localScale.x/2 &&
+			ball.transform.position.y - radius < box.transform.position.y + box.transform.localScale.y/2 &&
+			ball.transform.position.y + radius > box.transform.position.y - box.transform.localScale.y/2) {
+
+			//Ball hit right side
+			if (ball.transform.position.x > box.transform.position.x + box.transform.localScale.x/2) {
+				Debug.Log ("Right Side!");
+				ballDirection = new Vector2 (-ballDirection.x, ballDirection.y);
+				boxDirection = new Vector2 (-boxDirection.x, boxDirection.y);
+			}//Ball hit left side
+			else if (ball.transform.position.x < box.transform.position.x - box.transform.localScale.x/2) {
+				Debug.Log ("Left Side!");
+				ballDirection = new Vector2 (-ballDirection.x, ballDirection.y);
+				boxDirection = new Vector2 (-boxDirection.x, boxDirection.y);
+			}//Ball hit top
+			else if (ball.transform.position.y > box.transform.position.y + box.transform.localScale.y/2) {
+				Debug.Log ("Top!");
+				ballDirection = new Vector2 (ballDirection.x, -ballDirection.y);
+				boxDirection = new Vector2 (boxDirection.x, -boxDirection.y);
+			}
+			else if (ball.transform.position.y < box.transform.position.y - box.transform.localScale.y/2) {
+				Debug.Log ("Bottom!");
+				ballDirection = new Vector2 (ballDirection.x, -ballDirection.y);
+				boxDirection = new Vector2 (boxDirection.x, -boxDirection.y);
+			}
+		} //end ball collision with box
+
+
 	}
 		
 
